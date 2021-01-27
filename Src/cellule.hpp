af69724d964dde17cc4ceb4af90e961fec6db957
Froissart Kevin <kevin.froissart69@gmail.com>
2021-01-27 16:21:32 +0100

    Exercice test_liste terminé

--- Src/cellule.hpp
+++ Src/cellule.hpp
@@ -1,10 +1,16 @@
 #ifndef LIFAP6_LISTE_CELLULE_HPP
 #define LIFAP6_LISTE_CELLULE_HPP
 
-class Cellule {
+class Cellule
+{
 
-  /* votre code ici */
+public:
+  Cellule(int valeur, Cellule *ptr);
+  Cellule(int valeur);
+  int valeur;
+  Cellule *suivant;
 
-} ;
+private:
+};
 
 #endif
--- Src/liste.cpp
+++ Src/liste.cpp
@@ -2,71 +2,139 @@
 
 #include <iostream>
 #include <cassert>
+#include <string>
 
-Liste::Liste() {
-  /* votre code ici */
+Liste::Liste()
+{
+  taille_liste = 0;
+  first = nullptr;
+  last = first;
 }
 
-Liste::Liste(const Liste& autre) {
-  /* votre code ici */
+Liste::Liste(const Liste &autre)
+{
+  taille_liste = 0;
+  first = nullptr;
+  Cellule *tmp = autre.first;
+  while (tmp->suivant != nullptr)
+  {
+    ajouter_en_tete(tmp->valeur);
+    tmp = tmp->suivant;
+  }
+  ajouter_en_queue(tmp->valeur);
 }
 
-Liste& Liste::operator=(const Liste& autre) {
-  /* votre code ici */
-  return *this ;
+Liste &Liste::operator=(const Liste &autre)
+{
+  taille_liste = 0;
+  first = nullptr;
+  Cellule *tmp = autre.first;
+  while (tmp->suivant != nullptr)
+  {
+    ajouter_en_tete(tmp->valeur);
+    tmp = tmp->suivant;
+  }
+  ajouter_en_queue(tmp->valeur);
+  return *this;
 }
 
-Liste::~Liste() {
-  /* votre code ici */
+Liste::~Liste()
+{
+  while (first->suivant != nullptr)
+  {
+    supprimer_en_tete;
+  }
+  first->suivant == nullptr;
+  delete first;
+  delete last;
 }
 
-void Liste::ajouter_en_tete(int valeur) {
-  /* votre code ici */
+void Liste::ajouter_en_tete(int valeur)
+{
+  first = new Cellule(valeur, first);
+  if (taille_liste == 0)
+    last = first;
+  taille_liste++;
 }
 
-void Liste::ajouter_en_queue(int valeur) {
-  /* votre code ici */
+void Liste::ajouter_en_queue(int valeur)
+{
+  last->suivant = new Cellule(valeur, nullptr);
+  last = last->suivant;
+  taille_liste++;
 }
 
-void Liste::supprimer_en_tete() {
-  /* votre code ici */
+void Liste::supprimer_en_tete()
+{
+  if (first)
+  {
+    Cellule *tmp = first;
+    first = tmp->suivant;
+    tmp->suivant = nullptr;
+    taille_liste--;
+    delete tmp;
+  }
 }
 
-Cellule* Liste::tete() {
-  /* votre code ici */
-  return nullptr ;
+Cellule *Liste::tete()
+{
+  return first;
 }
 
-const Cellule* Liste::tete() const {
-  /* votre code ici */
-  return nullptr ;
+const Cellule *Liste::tete() const
+{
+  return first;
 }
 
-Cellule* Liste::queue() {
-  /* votre code ici */
-  return nullptr ;
+Cellule *Liste::queue()
+{
+  return last;
 }
 
-const Cellule* Liste::queue() const {
-  /* votre code ici */
-  return nullptr ;
+const Cellule *Liste::queue() const
+{
+  return last;
 }
 
-int Liste::taille() const {
-  /* votre code ici */
-  return 0 ;
+int Liste::taille() const
+{
+  return taille_liste;
 }
 
-Cellule* Liste::recherche(int valeur) {
-  /* votre code ici */
-  return nullptr ;
+Cellule *Liste::recherche(int valeur)
+{
+  Cellule *tmp = first;
+  for (int i = 0; i < taille_liste; i++)
+  {
+    if (tmp->valeur == valeur)
+      return tmp;
+    tmp = tmp->suivant;
+  }
+  return nullptr;
 }
 
-const Cellule* Liste::recherche(int valeur) const {
-  /* votre code ici */
-  return nullptr ;
+const Cellule *Liste::recherche(int valeur) const
+{
+  Cellule *tmp = first;
+  for (int i = 0; i < taille_liste; i++)
+  {
+    if (tmp->valeur == valeur)
+      return tmp;
+    tmp = tmp->suivant;
+  }
+  return nullptr;
 }
 
-void Liste::afficher() const {
-  /* votre code ici */
+void Liste::afficher() const
+{
+  std::string affichage = "";
+  Cellule *tmp = first;
+  affichage += "[ ";
+  for (int i = 0; i < taille_liste; i++)
+  {
+    affichage += std::to_string(tmp->valeur) + " ";
+    tmp = tmp->suivant;
+  }
+  affichage += "]";
+  std::cout << affichage << std::endl;
 }
